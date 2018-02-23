6e95d2e88bde8079a4fe34c83f8c97e886c999c3
horizon <dembotoure00@gmail.com>
2018-02-23 11:54:59 +0000

    fisrt version of server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+
+let express = require('express')
+let app = express()
+
+app.set('view engine' , 'ejs')
+app.use( '/assets' , express.static('public'))
+
+app.get( '/' , (request , response)=>{
+    response.render('pages/index')
+})
+
+app.get('/contributor' , (request , response)=>{
+    response.render('pages/contributor')
+})
+
+app.get('/blog' , (request, response)=>{
+    response.render('pages/blog')
+})
+
+app.get('/physic' , (request , response)=>{
+    response.render('pages/physic')
+})
+
+app.get('/maths' , (request,response)=>{
+    response.render('pages/maths')
+})
+
+app.get('/convert' , (request,response)=>{
+    response.render('pages/convert')
+})
+
+
+app.listen(8080)
+ 
